1dec991d71420f0da8087f10f3beb8dc3b7f2965
ababic20 <ababic20@foi.hr>
2023-12-13 21:49:16 +0100

    Implemented kafka consumer

---
+++ backend/kafka/consumer.js
@@ -0,0 +1,28 @@
+const { Kafka } = require("kafkajs");
+
+class Consumer {
+  constructor() {
+    this.groupId = "test-group"; 
+    this.kafka = new Kafka({
+      clientId: "test-streaming",
+      brokers: ["localhost:9092"],
+    });
+  }
+  async consume(req, res) {
+    const consumer = this.kafka.consumer({ groupId: this.groupId });
+
+    try {
+      await consumer.connect();
+      await consumer.subscribe({
+        topic: "test-streaming",
+        fromBeginning: true,
+      });
+      return consumer;
+    } catch (error) {
+      console.error("Error connecting to Kafka:", error);
+      throw error; 
+    }
+  }
+}
+
+module.exports = Consumer;
