69bc8b690c8ab347cdfb33507f1ba4d5114cacdf
Filip1402 <zutifilip@gmail.com>
2023-12-13 21:45:08 +0100

    Implemeted production of hls playlist to kafka topic

---
+++ backend/kafka/producer.js
@@ -0,0 +1,65 @@
+const { Kafka } = require("kafkajs");
+const { spawn } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+
+const kafka = new Kafka({
+    clientId: "test-streaming",
+    brokers: ["localhost:9092"],
+    requestTimeout: 250000,
+    connectionTimeout: 30000,
+});
+
+class Producer {
+
+    createSegmentKey(videoName, index) { 
+        return `${videoName}_${index}.ts`; //naming convention for video segments
+    }
+    produce = async function (req, res) {
+        const producer = kafka.producer({ maxMessageBytes: 5000000 });
+        await producer.connect();
+
+        const ffmpegProcess = spawn("ffmpeg", [
+            "-i", path.resolve(req.body.name),
+            "-c:a", "copy",
+            "-c:v", "copy",
+            "-movflags", "frag_keyframe+empty_moov+default_base_moof",
+            "-hls_time", "10",
+            "-hls_list_size", "0",
+            "-hls_segment_filename", `./video/${req.body.name.split('.')[0]}_%d.ts`,
+            "-f", "hls",
+            `./video/${req.body.name.split('.')[0]}.m3u8`
+        ]);
+        
+        ffmpegProcess.on("close", async (code) => {
+            if (code === 0) {
+                await new Promise(resolve => setTimeout(resolve, 2000));
+
+                const masterPlaylistPath = `./video/${req.body.name.split('.')[0]}.m3u8`;
+                if (fs.existsSync(masterPlaylistPath)) {
+                    const masterPlaylistData = fs.readFileSync(masterPlaylistPath,'utf-8');
+
+                    const key = {key: req.body.name.split('.')[0] + ".m3u8" + "_playlist"}
+                    await producer.send({
+                        topic: "test-streaming",
+                        messages: [
+                            {
+                                value: masterPlaylistData,
+                                key: JSON.stringify(key)
+                            },
+                        ],
+                    });
+                    fs.unlinkSync(masterPlaylistPath);
+                }
+
+            } else {
+                console.error(`ffmpeg process exited with code ${code}`);
+            }
+
+            await producer.disconnect();
+        });
+    };
+
+}
+
+module.exports = Producer;
