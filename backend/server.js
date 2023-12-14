bf61ec87cc3714d8ad31182f80648e6a4fc8c230
ababic20 <100703395+ababic20@users.noreply.github.com>
2023-12-14 21:04:10 +0100

    Server completed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const cors = require('cors');
-var stream = require("stream");
-var streamBuffers = require("stream-buffers");
+const Producer = require('./kafka/producer');
+const Consumer = require('./kafka/consumer');
+const producer = new Producer()
+const consumer = new Consumer()
 const app = express()
 const port = 3000
 
@@ -10,10 +12,61 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-var current = 0;
 var mes = [];
 
+app.post('/produce',async(req,res)=>{
+  producer.produce(req,res)
+  const cons = await consumer.consume(req,res);
+  cons.run({
+    eachMessage: ({message }) => {
+      console.log({
+        value: message.value,
+        key: JSON.parse(message.key),
+      });
+      const new_message = Object.assign({}, message);
+      //const start = 0;
+      //const end = message.value.length;
+      //const chunksize = end - start + 1;
+      mes.push(new_message);
+    },
+  });
+  res.setHeader("Content-type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.send({ message: "producing" });
+})
+
+app.get('/hlsPlaylist/', async (req, res) => {
+  res.setHeader('Content-type', 'application/vnd.apple.mpegurl');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const key = req.query.name.split('.')[0]+".m3u8_playlist"
+  const playlist = mes.find((message) => JSON.parse(message.key).key === key);
+  if (playlist) {
+    const buf_array = playlist.value;
+    const buf = Buffer.from(buf_array);
+    res.end(buf);
+  } else {
+    res.status(404).send('HLS playlist not found');
+  }
+});
+
+app.get('/:segment', async (req, res) => {
+  const key = req.params.segment
+  const message = mes.find((message) => JSON.parse(message.key).key === key);
+  if(message)
+  {
+    const buf_array = message.value;
+
+    const buffer = Buffer.from(buf_array);
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Length', buffer.length);
+    res.setHeader('Content-Type', 'video/mp4');
+
+    res.end(buffer);
+  } else {
+    res.status(404).send('Segment not found');
+  }
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
